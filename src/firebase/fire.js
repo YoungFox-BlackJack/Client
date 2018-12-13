10d0e000b47ac5107894cf75de60433d28783e49
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-12-13 17:45:27 +0700

    initial commit

---
+++ src/firebase/fire.js
@@ -0,0 +1,14 @@
+var firebase = require("firebase");
+import firebase from 'firebase'
+require('dotenv').config()
+var config = {
+    apiKey: process.env.API_KEY,
+    authDomain: "youngfox-blackjack.firebaseapp.com",
+    databaseURL: "https://youngfox-blackjack.firebaseio.com",
+    projectId: "youngfox-blackjack",
+    storageBucket: "youngfox-blackjack.appspot.com",
+    messagingSenderId: "82165920548"
+};
+const firebaseStart = firebase.initializeApp(config);
+
+export default firebaseStart
