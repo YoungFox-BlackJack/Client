19976f5e389395e3506ca57302194dec8fca5384
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-12-14 13:38:18 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-<<<<<<< HEAD
-=======
 
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
->>>>>>> backup of lobby
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
