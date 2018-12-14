4000cecd4a9c4abc228e2e0faf1886f6ac7b971a
javasusanto <herisusanto.id@gmail.com>
2018-12-14 10:27:00 +0700

    Get Card

--- src/firebase/fire.js
+++ src/firebase/fire.js
@@ -1,14 +1,18 @@
-var firebase = require("firebase");
 import firebase from 'firebase'
+// var firebase = require('firebase')
 require('dotenv').config()
 var config = {
-    apiKey: process.env.API_KEY,
-    authDomain: "youngfox-blackjack.firebaseapp.com",
-    databaseURL: "https://youngfox-blackjack.firebaseio.com",
-    projectId: "youngfox-blackjack",
-    storageBucket: "youngfox-blackjack.appspot.com",
-    messagingSenderId: "82165920548"
-};
-const firebaseStart = firebase.initializeApp(config);
+  apiKey: process.env.API_KEY,
+  authDomain: 'youngfox-blackjack.firebaseapp.com',
+  databaseURL: 'https://youngfox-blackjack.firebaseio.com',
+  projectId: 'youngfox-blackjack',
+  storageBucket: 'youngfox-blackjack.appspot.com',
+  messagingSenderId: '82165920548'
+}
+const firebaseStart = firebase.initializeApp(config)
 
 export default firebaseStart
+
+
+
+
