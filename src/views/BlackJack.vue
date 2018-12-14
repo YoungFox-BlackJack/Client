5e05847d7c4ec9ce16a92e6d1b94e32a6caf9f9a
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-12-14 15:49:39 +0700

    join-interface

--- src/views/BlackJack.vue
+++ src/views/BlackJack.vue
@@ -1,112 +1,234 @@
 <template>
   <div>
-    <b-button class="mr-3" :class="{disabled: isPlayerTurn}" @click="draw">Draw</b-button>
-    <b-button @click="pass">Pass</b-button>
-   <b-alert dismissible :show="readyAlert" :variant="alert"  @dismissed="readyAlert=false">{{message}}</b-alert>
-    <ul>
-      <li v-for="(card, index) in ownCards" :key="index">{{card.value}} </li>
-    </ul>
-     <ul>
-      <li v-for="(card, index) in opponentCards" :key="index">{{card}}</li>
-    </ul>
-</div>
+    <b-carousel
+      id="carousel1"
+      style="text-shadow: 1px 1px 2px #333;"
+      controls
+      indicators
+      background="#ababab"
+      :interval="4000"
+      img-width="1024"
+      img-height="150"
+      v-model="slide"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide img-blank>
+        <h5>
+          Peraturan : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </h5>
+      </b-carousel-slide>
+      <b-carousel-slide img-blank>
+        <H1>Game Still Runing .....</H1>
+      </b-carousel-slide>
+    </b-carousel>
+    <b-container class="bv-example-row" style="padding-top:10px">
+      <b-row>
+        <b-col col="2">
+          <div>
+            <b-card-group deck class="mb-3">
+              <b-card border-variant="dark" header="Chat Room" align="center">
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </b-card>
+            </b-card-group>
+          </div>
+        </b-col>
+        <b-col cols="5">
+          <div>
+            <b-card-group deck class="mb-3">
+              <b-card border-variant="dark" header="You" align="center">
+                <b-row>
+                  <div>
+                    <b-card-group>
+                      <b-card
+                        v-for="(card, index) in ownCards"
+                        :key="index"
+                        align="center"
+                        :img-src="card.image"
+                      ></b-card>
+                    </b-card-group>
+                  </div>
+                </b-row>
+                <!-- bottom -->
+                <b-row>
+                  <b-col>
+                    <div>
+                      <div>
+                        <!-- <b-button style="margin:20px">Pass</b-button>
+                        <b-button style="margin-left:10px ">Next</b-button>-->
+                      </div>
+                    </div>
+                  </b-col>
+                </b-row>
+                <!-- n-buttom -->
+              </b-card>
+            </b-card-group>
+          </div>
+        </b-col>
+        <b-col cols="5">
+          <div>
+            <b-card-group deck class="mb-3">
+              <b-card border-variant="dark" header="Opponent" align="center">
+                <b-row>
+                  <div>
+                    <b-card-group>
+                      <b-card
+                        v-for="(card, index) in opponentCards"
+                        :key="index"
+                        align="center"
+                        img-src="https://opengameart.org/sites/default/files/styles/medium/public/card%20back%20black.png"
+                      ></b-card>
+                    </b-card-group>
+                  </div>
+                </b-row>
+                <!-- bottom -->
+                <b-row>
+                  <b-col>
+                    <div></div>
+                  </b-col>
+                </b-row>
+                <!-- n-buttom -->
+              </b-card>
+            </b-card-group>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div>
+      <b-button
+        style="margin:20px"
+        class="mr-3"
+        :class="{disabled: isPlayerTurn}"
+        @click="draw"
+      >Draw</b-button>
+      <b-button style="margin-left:20px" @click="pass">Pass</b-button>
+
+      <b-alert
+        dismissible
+        :show="readyAlert"
+        :variant="alert"
+        @dismissed="readyAlert=false"
+      >{{message}}</b-alert>
+    </div>
+  </div>
 </template>
 <script>
-import firebase from "firebase"
-import axios from "axios"
+import firebase from "firebase";
+import axios from "axios";
 export default {
-  mounted () {
-    let playerRef = firebase.database().ref('players/' + localStorage.getItem('token'))
-    playerRef.on('value', (snapshot) => {
+  mounted() {
+    let playerRef = firebase
+      .database()
+      .ref("players/" + localStorage.getItem("token"));
+    playerRef.on("value", snapshot => {
       if (snapshot.val().turn) {
-        this.isPlayerTurn = false
+        this.isPlayerTurn = false;
       } else {
-        this.isPlayerTurn = true
+        this.isPlayerTurn = true;
       }
-    })
+    });
 
-    let cards = firebase.database().ref('players/' + localStorage.getItem('token') + '/cards')
-    cards.on('value', (snapshot) => {
-      this.ownCards = Object.values(snapshot.val())
-    })
+    let cards = firebase
+      .database()
+      .ref("players/" + localStorage.getItem("token") + "/cards");
+    cards.on("value", snapshot => {
+      this.ownCards = Object.values(snapshot.val());
+    });
 
-    let test = firebase.database().ref('players')
-    test.on('value', (snapshot, err) => {
-      this.gameEnd = 0
-        snapshot.forEach((childNode) => {
-          if(childNode.child('/done').val()) {
-            this.gameEnd ++
-          }
-        })
-    })
+    let test = firebase.database().ref("players");
+    test.on("value", (snapshot, err) => {
+      this.gameEnd = 0;
+      snapshot.forEach(childNode => {
+        if (childNode.child("/done").val()) {
+          this.gameEnd++;
+        }
+      });
+    });
 
-    let opponent = firebase.database().ref('players')
-    opponent.on('value', (snapshot, err) => {
+    let opponent = firebase.database().ref("players");
+    opponent.on("value", (snapshot, err) => {
       if (!err) {
-        snapshot.forEach((childNode) => {
-
-          if (childNode.key != localStorage.getItem('token')) {
-            this.opponentCards = []
-            childNode.child('/cards').forEach((superChild) => {
-                this.opponentCards.push('ga bole liat')
-            })
+        snapshot.forEach(childNode => {
+          if (childNode.key != localStorage.getItem("token")) {
+            this.opponentCards = [];
+            childNode.child("/cards").forEach(superChild => {
+              this.opponentCards.push("ga bole liat");
+            });
           }
-        })
+        });
       }
-    })
+    });
   },
   methods: {
-    pass () {
-      firebase.database().ref(`players/${localStorage.getItem('token')}`).update({
-        turn: false,
-        done: true
-        }, (error) => {
-          if (error) {
-            // this.message = 'Failed to ready check, please ready again'
-            // this.alert = 'danger'
-            // this.readyAlert = true
-          } else {
-             let players = firebase.database().ref('players')
-              players.on('value', (snapshot) => {
-                  snapshot.forEach((childNode) => {
-                    let playerDone = childNode.child('/done').val()
-                    if (childNode.key != localStorage.getItem('token') && !playerDone) {
-                      firebase.database().ref(`players/${childNode.key}`).update({
-                        turn: true
-                      }, (error) => {
+    pass() {
+      firebase
+        .database()
+        .ref(`players/${localStorage.getItem("token")}`)
+        .update(
+          {
+            turn: false,
+            done: true
+          },
+          error => {
+            if (error) {
+              // this.message = 'Failed to ready check, please ready again'
+              // this.alert = 'danger'
+              // this.readyAlert = true
+            } else {
+              let players = firebase.database().ref("players");
+              players.on("value", snapshot => {
+                snapshot.forEach(childNode => {
+                  let playerDone = childNode.child("/done").val();
+                  if (
+                    childNode.key != localStorage.getItem("token") &&
+                    !playerDone
+                  ) {
+                    firebase
+                      .database()
+                      .ref(`players/${childNode.key}`)
+                      .update(
+                        {
+                          turn: true
+                        },
+                        error => {
                           if (error) {
-
                           } else {
-
                           }
-                      })
-                    }
-                  })
-              })
+                        }
+                      );
+                  }
+                });
+              });
             }
-        })
+          }
+        );
     },
-    draw () {
-      let card = null
+    draw() {
+      let card = null;
       axios({
         method: "GET",
         url: `https://deckofcardsapi.com/api/deck/new/draw/?count=1`
-        })
+      })
         .then(cards => {
-          card = cards.data.cards[0]
-          firebase.database().ref(`/players/${localStorage.getItem('token')}/cards`).push(card)
+          card = cards.data.cards[0];
+          firebase
+            .database()
+            .ref(`/players/${localStorage.getItem("token")}/cards`)
+            .push(card);
         })
         .catch(err => {
-          console.log(err)
-      })
+          console.log(err);
+        });
     }
   },
   data() {
     return {
       turn: true,
       readyAlert: false,
-      message: '',
-      alert:'',
+      message: "",
+      alert: "",
       start: false,
       gameStart: false,
       totalReady: 0,
@@ -115,15 +237,15 @@ export default {
       ownCards: [],
       opponentCards: [],
       gameEnd: 0
-    }
+    };
   },
   watch: {
-    gameEnd (value) {
+    gameEnd(value) {
       if (value === 2) {
-        this.$router.push('/result')
+        this.$router.push("/result");
       }
     }
   }
-}
+};
 </script>
 
